da4bf90669c90fa46d23d0b612e5f70aba3c3830
malit mohamut <malitmohamud@gmai.com>
2023-05-03 10:26:05 +0300

    new commit

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("hello");
+// console.log("hello");
+
+let y = 10;
+let x = 20;
+
+if (x > y) {
+  console.log("X is greater than the Y");
+} else {
+  console.log("That is a fake number");
+}
